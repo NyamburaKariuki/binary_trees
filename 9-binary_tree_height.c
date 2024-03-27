9be6c573a29986f010173e50efa17dc32a111526
NyamburaKariuki <prettimercii@gmail.cm>
2024-03-27 22:50:50 +0300

    depth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		size_t right_h = tree->right ? 1+ binary_tree_height(tree->right) : 0;
+		size_t right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 		return ((left_h > right_h) ? left_h : right_h);
 	}
